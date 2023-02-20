885f9c55c3a5cc46994428a7f0fb77f01ed10121
haberger <d.haberger@hotmail.com>
2023-02-20 15:13:17 +0100

    compile for cleargrasp

---
+++ scripts/compile_metrics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import os
+
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+import sys
+from scipy.spatial.transform import Rotation as R
+from pathlib import Path
+from attrdict import AttrDict
+
+exps_dir = '/home/dalina/David/Uni/BachelorThesis/Dataset/scenes/014_only_canister/cleargrasp/results'
+exps = sorted(os.listdir(exps_dir))
+
+res = []
+
+for exp in exps:
+    exp_dir = os.path.join(exps_dir, exp)
+    CONFIG_FILE_PATH = os.path.join(exp_dir, 'config.yaml')
+    with open(CONFIG_FILE_PATH) as fd:
+        config_yaml = yaml.safe_load(fd)
+    config = AttrDict(config_yaml)
+
+    masked = os.path.join(exp_dir, 'metrics/', 'masked_metrics.csv')
+    unmasked = os.path.join(exp_dir, 'metrics/', 'unmasked_metrics.csv')
+    with open(masked, "r") as f1:
+        last_line_masked = f1.readlines()[-1]
+    with open(unmasked, "r") as f1:
+        last_line_unmasked = f1.readlines()[-1]    
+    
+    res.append(str(config['depth2depth']['xres']) + ',' +
+               str(config['depth2depth']['yres']) + ',' +
+               last_line_masked[:-1] + ',' +
+               last_line_unmasked[:-1] + ','
+               )
+for r in res:
+    print(r)
+
+    
